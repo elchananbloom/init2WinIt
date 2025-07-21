98b065560accdc24200a43820f266878b3aa2d4b
Jroman5 <jonroman2525@gmail.com>
2025-07-21 11:50:55 -0600

    implemented User controller and updated user service

---
+++ server/main/java/bank/controllers/UserController.java
@@ -0,0 +1,35 @@
+package bank.controllers;
+
+import bank.domain.Result;
+import bank.domain.UserService;
+import bank.models.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/api/user")
+public class UserController {
+
+    private UserService service;
+
+    public UserController(UserService userService){
+        this.service = userService;
+    }
+
+    @GetMapping("/{userId}")
+    public User findById(@PathVariable int userId){
+        return service.findUserById(userId);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody User user){
+        Result<User> result = service.addUser(user);
+        if(!result.isSuccess()){
+            ErrorResponse.build(result);
+        }
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+    }
+
+}

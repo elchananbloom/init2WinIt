fb3db5131e7d30fe6f0b386b31a1061e232d48f4
elchananbloom <elchananbloom12@gmail.com>
2025-07-21 19:51:20 -0600

    Fixed account repository issues

--- server/main/java/bank/data/mappers/AccountMapper.java
+++ server/main/java/bank/data/mappers/AccountMapper.java
@@ -1,6 +1,7 @@
 package bank.data.mappers;
 
 import bank.models.Account;
+import bank.models.AccountType;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
@@ -12,11 +13,12 @@ public class AccountMapper implements RowMapper<Account> {
     public Account mapRow(ResultSet resultSet, int i) throws SQLException {
         Account account = new Account();
         account.setAccountId(resultSet.getInt("account_id"));
+        account.setAccountType(AccountType.findByValue(resultSet.getString("type")));
         account.setBalance(resultSet.getBigDecimal("balance"));
         account.setAccountNumber(resultSet.getString("account_number"));
         account.setCreatedAt(resultSet.getDate("created_at").toLocalDate());
         account.setUserId(resultSet.getInt("user_id"));
 
-        return null;
+        return account;
     }
 }

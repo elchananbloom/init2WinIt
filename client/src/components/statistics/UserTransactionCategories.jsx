0c77cc069f94da6954a2033d016bd5dea2d56eaa
Jroman5 <jonroman2525@gmail.com>
2025-07-23 10:44:03 -0600

    updated server classes with proper endpoints and created components for statistics page

---
+++ client/src/components/statistics/UserTransactionCategories.jsx
@@ -0,0 +1,57 @@
+import { colors } from "@mui/material";
+import { BarChart } from '@mui/x-charts/BarChart';
+import {PieChart } from '@mui/x-charts/PieChart';
+import {useEffect, useState, useContext} from 'react';
+import UserContext from "../../contexts/UserContext";
+
+function UserTransactionCategories(){
+
+    const defaultData = [
+        { label: 'Group A', value: 400, color: '#0088FE' },
+        { label: 'Group B', value: 300, color: '#00C49F' }, 
+        { label: 'Group C', value: 300, color: '#FFBB28' },
+        { label: 'Group D', value: 200, color: '#FF8042' },
+    ];   
+
+    const [data , setData] = useState(defaultData);
+    const user = useContext(UserContext);
+    const url = `http://localhost:8080/api/statistics/user/category_average/${user.userId}`;
+    
+    
+    useEffect(() => {
+        fetch(url).then(response => {
+            if(response.status === 200){
+                return response.json();
+            } else{
+                return Promise.reject(`Encountered unexpected status: ${response.status}`);
+            }
+        }).then(data => {
+
+            const res = [];
+            for (const [key, value] of Object.entries(data)) {
+                res.push({label: key, value: value});
+                console.log(`${key}: ${value}`);
+            }
+
+            res.map((k,v) => console.log(`${k}:${v}`));
+            setData(res);
+        
+        })
+        .catch(console.log);
+    },[]);
+
+const settings = {
+  margin: { right: 5 },
+  width: 200,
+  height: 200,
+  hideLegend: true,
+};
+
+    return <>
+                <PieChart
+                    series={[{ innerRadius: 50, outerRadius: 100, data, arcLabel: 'value' }]}
+                    {...settings}
+                />
+            </>
+}
+export default UserTransactionCategories;
--- client/src/pages/StatisticsPage.jsx
+++ client/src/pages/StatisticsPage.jsx
@@ -1,8 +1,27 @@
-import Page from "../components/Page"
+import { colors } from "@mui/material";
+
+import { BarChart } from '@mui/x-charts/BarChart';
+import {PieChart } from '@mui/x-charts/PieChart';
+import {useEffect, useState} from 'react';
+import CategoryTransactions from "../components/statistics/CategoryTransactions";
+import UserTransactionCategories from "../components/statistics/UserTransactionCategories";
+import Page from "../components/Page";
+import TotalBalanceAcrossAccounts from "../components/statistics/TotalBalanceAcrossAccounts";
+
 
 
 const StatisticsPage = () => {
+
+
+
     return (<Page>
+        
+        <CategoryTransactions/>
+        <UserTransactionCategories/>
+        <TotalBalanceAcrossAccounts/>
+
+    
+
         
     </Page>);
 }
